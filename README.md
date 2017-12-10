406e3287d143151516a300160583bbdcb7cbfe84
evle <evlefsp@163.com>
2017-12-11 00:35:22 +0800

    code base

--- README.md
+++ README.md
@@ -1,17 +1,98 @@
-# Min
+# Zurb Foundation
 
-Min is a elegant theme [Hexo](http://hexo.io/) theme, create with [Zurb Foundation 5+] and [Sass]().
+A brand new default theme for [Hexo]. This theme is <b>Work in progress</b>. It utilizes Zurb Foundation 5+.
 
-- [Live Demo](http://evle.github.io)
+- [Preview](http://hexo.io/themes/)
+- [Demo](http://chrisjlee.github.io/hexo-theme-zurb-foundation/)
 
 ## Installation
 
-1. Download the theme from GitHub
+### Install
+
 ``` bash
-$ git clone git@github.com:evle/min.git
+$ git clone https://github.com/chrisjlee/hexo-theme-zurb-foundation.git
 ```
 
-2. Change `_config.yml`
-```yml
-theme: min
-``` 
+**Landscape requires Hexo 3.0 and above.**
+
+### Enable
+
+Modify `theme` setting in `_config.yml` to `zurb-foundation`.
+
+### Update
+
+``` bash
+cd themes/zurb-foundation
+git pull
+```
+
+## Configuration
+
+``` yml
+# Header
+menu:
+  Home: /
+  Archives: /archives
+rss: /atom.xml
+
+# Content
+excerpt_link: Read More
+fancybox: true
+
+# Sidebar
+sidebar: right
+widgets:
+- category
+- tag
+- tagcloud
+- archives
+- recent_posts
+
+# Miscellaneous
+google_analytics:
+favicon: /favicon.png
+twitter:
+google_plus:
+```
+
+- **menu** - Navigation menu
+- **rss** - RSS link
+- **excerpt_link** - "Read More" link at the bottom of excerpted articles. `false` to hide the link.
+- **fancybox** - Enable [Fancybox]
+- **sidebar** - Sidebar style. You can choose `left`, `right`, `bottom` or `false`.
+- **widgets** - Widgets displaying in sidebar
+- **google_analytics** - Google Analytics ID
+- **favicon** - Favicon path
+- **twitter** - Twiiter ID
+- **google_plus** - Google+ ID
+
+### Layouts
+
+Foundation provides 5 built-in widgets:
+
+- category
+- tag
+- tagcloud
+- archives
+- recent_posts
+
+All of them are enabled by default. You can edit them in `widget` setting.
+
+## Development
+
+Go ahead and use browser sync:
+
+```
+browser-sync start --server='../../public' --files="public/*.html,public/*.css"
+```
+
+In order to get started with hexo use gulp.
+
+```
+hexo generate --watch
+```
+
+### Requirements
+
+- [Grunt] 0.4+
+- Hexo 3.0+
--- gulpfile.js
+++ gulpfile.js
@@ -1,85 +1,20 @@
-var gulp        = require('gulp'),
-    browserSync = require('browser-sync'),
-    sass        = require('gulp-sass'),
-    bower       = require('gulp-bower'),
-    notify      = require('gulp-notify'),
-    reload      = browserSync.reload,
-    bs          = require("browser-sync").create(),
-    Hexo        = require('hexo'),
-    hexo        = new Hexo(process.cwd(), {}),
-    clean = require('gulp-clean');
+'use strict';
 
-var src = {
-    scss: './scss/',
-    css:  './source/css',
-    ejs: 'layout'
-},
-watchFiles = [
-    './scss/*.scss',
-    '*/*.ejs'
-];
+var gulp = require('gulp');
+var sass = require('gulp-sass');
 
-
-// Static Server + watching scss/html files
-gulp.task('serve', ['sass:watch'], function() {
-
-    hexo.init.then(function(){
-      return hexo.call('generate', {watch: true});
-    }).catch(function(err){
-      console.log(err);
-    });
-
-    // init starts the server
-    bs.init(watchFiles, {
-        server: {
-            baseDir: "../../public"
-        },
-        logLevel: "debug"
-    });
-
-    hexo.init.then(function(){
-      return hexo.call('generate', {watch: true});
-    }).catch(function(err){
-      console.log(err);
-    });
-
+gulp.task('sass', function () {
+  return gulp.src('./scss/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./source/css'));
 });
 
-
-
-gulp.task('build', ['clean', 'copy']);
-
-gulp.task('copy', function (cb) {
-    gulp.src(['../../public/**/*'])
-        .pipe(gulp.dest('public'));
-
+gulp.task('watch', function () {
+  gulp.watch('./scss/*.scss', ['sass']);
 });
 
-gulp.task('clean', function () {
-    return gulp.src('public', {read: false})
-            .pipe(clean({force: true}))
+gulp.task('copy', function(){
+    return gulp.src('./**/*', {base:"."})
+      .pipe(gulp.dest('/Users/evle//github/min/'));
 });
 
-
-
-// Compile sass into CSS
-gulp.task('sass', function() {
-    // gulp.src(src.scss + "/*/*.scss")
-    gulp.src(src.scss + "{,*}/*.scss")
-        .pipe(sass({}))
-        // .pipe(gulp.dest(src.css))
-        .pipe(gulp.dest('../../source/css/'))
-        .pipe(reload({stream: true}));
-});
-
-gulp.task('sass:watch', function () {
-  gulp.watch('./scss/**/*.scss', ['sass']);
-});
-
-
-gulp.task('bower', function() {
-  return bower()
-    .pipe(gulp.dest('source/lib'))
-});
-
-gulp.task('default', ['serve']);

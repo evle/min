1cf10ead68869fbb34666324a2737d33c478b207
evle <evlefsp@163.com>
2017-12-12 01:15:15 +0800

    stylish

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,22 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+// var browserSync = require('browser-sync').create();
+// gulp.task('browserSync', function() {
+//     browserSync.init({
+//          server: {
+//              baseDir: './'
+//          },
+//     });
+// });
 
 gulp.task('sass', function () {
   return gulp.src('./scss/*.scss')
     .pipe(sass.sync().on('error', sass.logError))
-    .pipe(gulp.dest('./source/css'));
+    .pipe(gulp.dest('./source/css'))
+    // .pipe(browserSync.reload({
+        // stream: true
+    // }));
 });
 
 gulp.task('watch', function () {
